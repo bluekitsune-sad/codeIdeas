02f6e9d923c26ad30748865587a57640081b8b7d
Saad Yousuf <116144344+bluekitsune-sad@users.noreply.github.com>
2025-09-23 13:30:30 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,45 +1,12 @@
-from PIL import Image
+from image_to_ascii import ImageToAscii  
 
-def image_to_characters(image_path, white_char='c', black_char='b', output_file='output.txt', new_size=(64, 64)):
-    # Open the image
-    img = Image.open(image_path)
+def main():
+    image_path = 'C:/Users/HuTa0710/Desktop/dontknowbutcute.jpeg'  
+    output_file = 'output_image.txt'  
     
-    # Resize the image to 256x256
-    img = img.resize(new_size)
+    image_to_ascii = ImageToAscii(image_path, output_file=output_file)
     
-    # Convert image to grayscale
-    grayscale_img = img.convert("L")
-    
-    # Get the pixel data
-    pixels = grayscale_img.load()
-    
-    # Get the dimensions of the image (should be 256x256)
-    width, height = grayscale_img.size
-    
-    # Loop through each pixel and convert it to a character
-    result = []
-    for y in range(height):
-        row = []
-        for x in range(width):
-            pixel_value = pixels[x, y]
-            
-            # If pixel is closer to white, set it to 'c', else 'b'
-            if pixel_value > 128:  # Pixel value ranges from 0 (black) to 255 (white)
-                row.append(white_char)  # White
-            else:
-                row.append(black_char)  # Black
-        result.append(''.join(row))
-    
-    # Join all rows into one string
-    ascii_art = '\n'.join(result)
-    
-    # Save the ASCII art into a text file
-    with open(output_file, 'w') as f:
-        f.write(ascii_art)
-    
-    print(f"ASCII art saved to {output_file}")
+    image_to_ascii.convert()
 
-# Example usage:
-image_path = 'C:/Users/HuTa0710/Desktop/dontknowbutcute.jpeg'  # Replace with the path to your image
-output_file = 'output_image.txt'  # The file where the ASCII art will be saved
-image_to_characters(image_path, output_file=output_file)
+if __name__ == "__main__":
+    main()
